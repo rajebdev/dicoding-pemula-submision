f8fae1cae634d774b56d3cd10bc62c3c5e8095a3
rajebdev <wijanarko.rajeb@gmail.com>
2019-08-14 16:39:16 +0700

    Add menu in every activity

--- app/src/main/java/com/bablaskoding/wasisjayagorden/AboutActivity.java
+++ app/src/main/java/com/bablaskoding/wasisjayagorden/AboutActivity.java
@@ -1,7 +1,10 @@
 package com.bablaskoding.wasisjayagorden;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         setTitle("About Me");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        setMode(item.getItemId());
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setMode(int selectedMode) {
+        switch (selectedMode) {
+            case R.id.action_home:
+                Intent homeActivity = new Intent(AboutActivity.this, MainActivity.class);
+                startActivity(homeActivity);
+                break;
+
+            case R.id.action_about:
+                break;
+
+        }
     }
 }
--- app/src/main/java/com/bablaskoding/wasisjayagorden/DetailActivity.java
+++ app/src/main/java/com/bablaskoding/wasisjayagorden/DetailActivity.java
@@ -1,7 +1,10 @@
 package com.bablaskoding.wasisjayagorden;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -41,5 +44,32 @@ public class DetailActivity extends AppCompatActivity {
         tvDataType.setText(type);
         tvDataPrice.setText(price);
         tvDataRating.setText(rating);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        setMode(item.getItemId());
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setMode(int selectedMode) {
+        switch (selectedMode) {
+            case R.id.action_home:
+                Intent homeActivity = new Intent(DetailActivity.this, MainActivity.class);
+                startActivity(homeActivity);
+                break;
+
+            case R.id.action_about:
+                Intent aboutActivity = new Intent(DetailActivity.this, AboutActivity.class);
+                startActivity(aboutActivity);
+                break;
+
+        }
     }
 }
